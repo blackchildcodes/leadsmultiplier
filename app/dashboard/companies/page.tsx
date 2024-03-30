862410e23095200071288465ab55c1a4e2d18cdc
blackchildcodes <blackchildspace@gmail.com>
2024-03-30 22:23:28 +0400

    Fixed key issue

--- app/dashboard/companies/page.tsx
+++ app/dashboard/companies/page.tsx
@@ -74,7 +74,7 @@ const CompaniesPage = () => {
         const options = {
             method: 'GET',
             headers: {
-                'X-RapidAPI-Key': 'a8c31c6278msha5be81b01cf38edp17b795jsn9821bb2152db',
+                'X-RapidAPI-Key': 'a969585268msh55e2bf4093fc67fp1d64fajsn2f2d47b4caf0',
                 'X-RapidAPI-Host': 'apollo-io1.p.rapidapi.com'
             }
         };
@@ -162,7 +162,7 @@ const CompaniesPage = () => {
                                 <SelectGroup>
                                     <SelectLabel>Select Employees Count</SelectLabel>
                                     {industries?.map((item) => (
-                                        <SelectItem value={item.id}>{item.cleaned_name}</SelectItem>
+                                        <SelectItem key={item.id} value={item.id}>{item.cleaned_name}</SelectItem>
                                     ))}
                                 </SelectGroup>
                             </SelectContent>
